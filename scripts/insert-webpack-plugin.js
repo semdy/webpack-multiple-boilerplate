96c72edf4e97aab4fb3ad7ff84c0e6abde252470
semdy <501859275@qq.com>
2019-06-11 12:27:54 +0800

    内联js处理

--- scripts/insert-webpack-plugin.js
+++ scripts/insert-webpack-plugin.js
@@ -12,7 +12,7 @@ InsertWebpackPlugin.prototype.apply = function(compiler) {
     compilation.plugin('html-webpack-plugin-after-html-processing', function(htmlPluginData, callback) {
       try {
         let inlineData = fs.readFileSync(pathInfo, 'utf8')
-        inlineData = UglifyJS.minify(`(function(){${inlineData}})()`).code || ''
+        inlineData = UglifyJS.minify(`(function(){${inlineData}})()`, { ie8: true }).code || ''
         htmlPluginData.html = htmlPluginData.html.replace('</head>', `\n<script>${inlineData}</script>\n</head>`)
         callback(null, htmlPluginData)
       } catch (e) {
